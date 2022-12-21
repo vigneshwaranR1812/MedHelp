3762f9205539895d5ea31e0fee33974c753cf051
Sai-2002 <smnjr2002@gmail.com>
2022-12-21 23:02:26 +0530

    Changed web.xml and finished FilterMed

--- java/FilterMed.java
+++ java/FilterMed.java
@@ -9,5 +9,17 @@ public class FilterMed extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 //        medtype comp manufactured by age uses
         String medType = req.getParameter("medType");
+        String composition = req.getParameter("composition");
+        String manufacturedBy = req.getParameter("manufacturedBy");
+        int age = Integer.parseInt(req.getParameter("age"));
+        String uses = req.getParameter("uses");
+
+        System.out.println("medType : "+medType+" "+"composition : "+composition+" "+"manufacturedBy : "+manufacturedBy+" "+"age : "+age+" "+"uses : "+uses+" ");
+        try{
+            resp.sendRedirect( "../webapp/medion/search.jsp?medType="+medType+"composition="+composition+"manufacturedBy="+manufacturedBy+"age="+age+"uses="+uses);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
