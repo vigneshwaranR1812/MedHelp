17b718bcdb709af1edf7145bb8da33b3bdea5219
Sai-2002 <smnjr2002@gmail.com>
2023-01-08 14:30:29 +0530

    Version-1 filter and search functionality

--- java/FilterMed.java
+++ java/FilterMed.java
@@ -2,6 +2,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -16,9 +17,17 @@ public class FilterMed extends HttpServlet {
         String lowerAge = req.getParameter("lowerAge");
         String upperAge = req.getParameter("upperAge");
         String uses = req.getParameter("uses");
-
-        System.out.println("medType : "+medtype+" "+"composition : "+medComposition+" ");
+//        Adding into session and redirect to search.jsp
         try{
+            HttpSession session = req.getSession();
+            session.setAttribute("medName", medName);
+            session.setAttribute("medType", medtype);
+            session.setAttribute("medComposition", medComposition);
+            session.setAttribute("manufacturedby", manufacturedby);
+            session.setAttribute("price", price);
+            session.setAttribute("lowerAge", lowerAge);
+            session.setAttribute("upperAge", upperAge);
+            session.setAttribute("uses", uses);
             resp.sendRedirect( "../search.jsp?medName="+medName+"medtype="+medtype+"medComposition="+medComposition+"manufacturedby="+manufacturedby+"price="+price+"lowerAge="+lowerAge+"upperAge="+upperAge+"uses="+uses);
         }
         catch (Exception e){
